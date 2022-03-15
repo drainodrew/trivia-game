a10b5d848e53ecb5984b7b5a2aa782ae817eafb1
Andrew Manuel <andrewmanuel@Andrews-MacBook-Pro.local>
2022-03-15 16:48:28 -0400

    added js, le fluer video background, and made it t

---
+++ script.js
@@ -0,0 +1,42 @@
+
+let htmlChoices = document.querySelectorAll(".choices")
+let htmlQuestions = document.querySelector(".prompt")
+let htmlScore = document.querySelector(".score")
+let htmlTimer = document.querySelector(".timer")
+
+//need to sort the array to be random every time
+const questions = [
+  {
+    question: "What is Tyler, the Creator's given full name?",
+    choices: ["Tyler Gregory Okonma","Tyler Elijah O'Neal","Tyler Elijah Okonma","Tyler Haley Okonma"],
+    answer: 0
+  },
+]
+
+for (i = 0, i < htmlChoices.length, i++) {
+  
+}
+
+console.log(questions)
+  
+let bastard = ["Bastard", "Seven", "Odd Toddlers",	"French!", "Blow", "Pigs Fly", "Parade", "Slow It Down","AssMilk", "VCR / Wheels", "Session", "Sarah", "Jack and the Beanstalk", "Tina", "Inglorious"]
+
+
+//   answer: [""]
+// }
+// let question2 = "In August 2016, what did Tyler say he would do if he were white for one day?"
+// let answer2 = ["Say the 'n word'", "Dye his hair blue", "Shot his shot with Biebs", "Play golf at the country club"]
+
+// let question3 = "What was the name of Tyler's app?"
+// let answer3 = ["Golf Wang", "WUPHF!", "Golf Media", ""]
+// ]
+
+// "What was T arrested for in 2014?"
+//   Inciting a riot
+// //for album i can do if right song in right order == green ; if wrong = red ; if right song, wrong order == yellow//
+
+// //who did not feature on this album ? //
+
+// //name "this" song --> play a random song in the background//
+
+//create an audio file of tyler saying "it's" and then him saying "answer" from the song --> it should play when you get the right answer
