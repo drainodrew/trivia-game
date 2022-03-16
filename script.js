8d66b715ddd17b3cd452ad3d7c5647129714dc5a
Andrew Manuel <andrewmanuel@Andrews-MacBook-Pro.local>
2022-03-16 16:49:17 -0400

    mainly css

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ let htmlScore = document.querySelector(".score")
 let htmlTimer = document.querySelector(".timer")
 let gameChoices = ''
 let score = 0;
-let startGame = document.querySelector(".starth1")
+let startGame = document.querySelector(".start-game")
+let headerTitle = document.querySelector(".header-title")
 
 //need to sort the array to be random every time
 const level1Questions = [
@@ -15,7 +16,7 @@ const level1Questions = [
     answer: 0
   },
   {
-    question: "when did he start OF?",
+    question: "When did he start OF?",
     choices: ["2009", "2006", "2007", "2005"],
     answer: 2
   }
@@ -30,17 +31,30 @@ const level1Questions = [
            // if the value of the element that was chosen == that object's (this.answer?) answer value --> then create animation (for now i'll do color) ... also change the prompt to say "correct" and queue a sound
               //if it's wrong do the same as above but with differnt content (tyler saying "fuck you")
 //functions 
+console.log(startGame)
+startGame.addEventListener("submit", function start(e) {
+  e.preventDefault();
+  startGame.style.display = "none"
+  headerTitle.style.display = "none"
+  // add transition function // 
+  level1Questions.forEach(question => {
+    htmlPrompt.innerHTML = question.question;
+  
+  })
+})
+
+//there need to be a function that sets the next prompts 
 
 
-for (let i = 0; i < htmlChoices.length; i++) {
-  htmlChoices[i].addEventListener("click", function idkYet(e) {
-    e.preventDefault;
-    level1Questions.forEach(question => {
-      htmlPrompt.innerHTML = question.question;
-  })
-console.log(htmlPrompt)
-  })
-}
+// for (let i = 0; i < htmlChoices.length; i++) {
+//   htmlChoices[i].addEventListener("click", function idkYet(e) {
+//     e.preventDefault;
+//     level1Questions.forEach(question => {
+//       htmlPrompt.innerHTML = question.question;
+//   })
+// console.log(htmlPrompt)
+//   })
+// }
 // level1Questions.forEach(question => {
 //   gameChoices = htmlChoices[question.question];
 //   console.log(gameChoices)
